0b877b09af6286b0d8d24359413788bb95a2748e
ScMohanty <92923028+ScMohanty@users.noreply.github.com>
2021-11-03 10:21:05 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Company
+    {
+        private string name;
+        private string sector;
+        private string location;
+        private int age;
+
+        public Company(string name, string sector, string location, int age)
+        {
+            Name = name;
+            Sector = sector;
+            Location = location;
+            Age = age;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Sector { get => sector; set => sector = value; }
+        public string Location { get => location; set => location = value; }
+        public int Age { get => age; set => age = value; }
+
+
+        public override string ToString()
+        {
+            return string.Format("Name {0}/nSector {1}/nLocation  {2}/nAge  {3}", Name, Sector, Location);
+        }
+        public override bool Equals(object obj)
+        {
+            Company c = (Company)obj;
+            return this.Name.Equals(c.Name) && this.Location.Equals(c.Location);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+
+    }
+   
+}
+    
+
+
---
+++ Program3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+       class Program3
+        {
+            public static void Main(string[] args)
+            {
+
+                int no_Details = 4;
+                int no_Company = 2;
+
+                string c_Name;
+                string c_Sector;
+                string c_Location;
+                int c_Age;
+                string company;
+
+
+                Company[] cArr = new Company[no_Details];
+                for (int i = 0; i < no_Company; i++)
+
+                {
+                    Console.WriteLine("ENTER THE NAME");
+                    c_Name = Console.ReadLine();
+                    Console.WriteLine("ENTER SECTOR");
+                    c_Sector = Console.ReadLine();
+                    Console.WriteLine("ENTER LOCATION");
+                    c_Location = Console.ReadLine();
+                    Console.WriteLine("ENTER AGE");
+                    c_Age = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+                    Company c = new Company(c_Name, c_Sector, c_Location, c_Age);
+
+
+                    if (!cArr.Contains(c))
+                    {
+                        cArr[i] = c;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Both the company are same");
+                    }
+
+
+                }
+
+                foreach (var item in cArr)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+              
+
+            }
+        }
+    }
+
